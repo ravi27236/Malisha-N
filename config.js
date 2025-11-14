9c0b879c8b704718019a2067373f86bd20d51d92
ravi27236 <oshadamd05@gmail.com>
2025-11-14 20:56:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
-if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-function convertToBool(text, fault = 'true') {
-    return text === fault ? true : false;
+if (fs.existsSync('config.env')) require('dotenv').confg({ path: './config.env' });
+function convertToBool(text, fault = 'true') 
+    return text === fault ? 
 }
 
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙽𝙰𝙳𝙴𝙴𝙽-𝙼𝙳=CYNVVCSB#yA5QGLytAQBl4SnxooY9Sp7VjGiPzC_VUd-8_yMsr7Q' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙽𝙰𝙳𝙴𝙴𝙽-𝙼𝙳=xcJxTZqZ#T2ChzR3Pm_Uiqa05HJ9-JeD_N1vvRyrMzO5S6VZ4p_s' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT, 
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'nadeenmd' : process.env.SESSION_NAME
 };
